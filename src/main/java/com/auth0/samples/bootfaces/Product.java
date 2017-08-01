d99fcbad36159e6d7cd43a73580cc75129d350e0
Szabados Alpár <szabadosalpar@gmail.com>
2017-08-02 00:37:20 +0300

    Rearranged methods

--- src/main/java/com/auth0/samples/bootfaces/Product.java
+++ src/main/java/com/auth0/samples/bootfaces/Product.java
@@ -1,10 +1,6 @@
 package com.auth0.samples.bootfaces;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.math.BigDecimal;
 import java.util.Objects;
 
@@ -32,27 +28,26 @@ public class Product {
 		return id;
 	}
 
-	public String getName() {
-		return name;
-	}
-
-	public BigDecimal getPrice() {
-		return price;
-	}
-
 	public void setId(Long id) {
 		this.id = id;
+	}
+
+	public String getName() {
+		return name;
 	}
 
 	public void setName(String name) {
 		this.name = name;
 	}
 
+	public BigDecimal getPrice() {
+		return price;
+	}
+
 	public void setPrice(BigDecimal price) {
 		this.price = price;
 	}
 
-	//TODO not necessary?
 	@Override
 	public boolean equals(Object o) {
 		if (this == o) return true;
